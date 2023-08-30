9454d4e7341b7c3d5efb0c561d6507a9b9822786
yudong-yeob <maintain98@naver.com>
2023-08-31 01:28:23 +0900

    create project

---
+++ src/test/java/project/MatchingTown/MatchingTownApplicationTests.java
@@ -0,0 +1,13 @@
+package project.MatchingTown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingTownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
