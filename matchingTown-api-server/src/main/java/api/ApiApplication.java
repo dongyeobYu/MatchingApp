be24b2f00f359699c51fbddd5fc1dad1b1b10fae
yudong-yeob <maintain98@naver.com>
2023-09-05 19:35:26 +0900

    refactor: add domain packages

--- matchingTown-api-server/src/main/java/api/ApiApplication.java
+++ matchingTown-api-server/src/main/java/api/ApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 // BaseScan을 core 모듈 하기
 @SpringBootApplication(scanBasePackages = "core")
 // EntityScan 으로 Core 모듈 스캔
-@EntityScan(basePackages = "core.entity")
+@EntityScan(basePackages = "core.domain.entity")
 public class ApiApplication{
 
     public static void main(String[] args) {
